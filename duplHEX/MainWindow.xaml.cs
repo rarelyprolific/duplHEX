a440cf7eb0a5cf2ae77520d2dd75e8acc82d91ae
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2019-10-29 22:27:18 +0000

    Added basic XAML grid and Close button

--- duplHEX/MainWindow.xaml.cs
+++ duplHEX/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace duplHEX
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
