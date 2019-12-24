8ff03ab78a0bc57a1e736fa89f9dab1ef39c67d0
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2019-12-24 14:08:33 +0000

    Fixed padding for hex viewer

--- duplHEX/HexBuilder.cs
+++ duplHEX/HexBuilder.cs
@@ -4,8 +4,13 @@ using System.Text.RegularExpressions;
 
 namespace duplHEX
 {
+    /// <summary>
+    /// Builds the hex display view from the raw bytes of a file
+    /// </summary>
     public class HexBuilder
     {
+        private const char SingleSpace = ' ';
+
         public string BuildHex(ReadOnlySpan<byte> fileBytes)
         {
             var hex = new StringBuilder();
@@ -19,7 +24,16 @@ namespace duplHEX
                 // Grabs the next 16 bytes for a row (unless we're on the last row and so we just get the remaining bytes)
                 var rowBytes = rowCount != i ? fileBytes.Slice(byteOffset, 16) : fileBytes.Slice(byteOffset);
 
-                hex.AppendLine($"{rowNumber}  {BuildHexRow(rowBytes)}  {BuildAscii(rowBytes)}");
+                // Calculate the white space padding to append to the end of the hex value to align the ASCII view
+                // (Really hacky (but fine for proof of concept).. Tidy this up later when I've got the essentials working.)
+                string spacePadding = string.Empty;
+                if (rowCount == i)
+                {
+                    spacePadding = spacePadding.PadRight((16 - (fileBytes.Length - byteOffset)) * 4);
+                }
+
+                // Builds a single row to display 16 bytes of the file
+                hex.AppendLine($"{rowNumber.ToString().PadRight(10, SingleSpace)}  {BuildHexRow(rowBytes)} {spacePadding} {BuildAscii(rowBytes)}");
 
                 rowNumber += 20;
                 byteOffset += 16;
